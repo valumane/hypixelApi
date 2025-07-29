07ae267ddc3eab19e9f88c51e698c0627264827c
Lucas V <lucas.vade@hotmail.com>
2025-07-29 15:11:07 +0200

    updt

--- item_gestion/gestion_head/indexbytexture.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-
-const dir = fs.readdirSync('./checkgoodformat/img');
-const filename = process.argv[2];
-
-const index = dir.indexOf(filename);
-console.log(index + 1);
--- item_gestion/gestion_head/texturebyindex.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs'
-
-
-
-const dir = fs.readdirSync('./checkgoodformat/img')
-const index = process.argv[2]
-console.log('.\\checkgoodformat\\img\\'+dir[index-1])
